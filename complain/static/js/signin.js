0c34c0f1b9204bc3b2db4a9a0226a15cba934a9d
newsujan <newsujans@gmail.com>
2016-03-12 19:55:07 +0545

    design change on signin page

--- complain/static/js/signin.js
+++ complain/static/js/signin.js
@@ -1,23 +1,27 @@
-$( "#signup" ).hide("fast");
 
+$( "#signup" ).hide("fast");
 $( "#click-signup" ).click(function() {
   $( "#signin" ).hide( "fast");
-  	$( "#signup" ).show("fast");
+  $(".box-signin").css( "margin-top", "20%" );
+  $( "#signup" ).show("fast");
+
 
 });
 $( "#click-signin" ).click(function() {
+  $(".box-signin").css( "margin-top", "30%" );
 	$( "#signup" ).hide("fast");
-
 	$( "#signin" ).show("fast");
 
 });
 
 $( "#click-signup-link" ).click(function() {
   $( "#signin" ).hide( "fast");
-  	$( "#signup" ).show("fast");
+  $(".box-signin").css( "margin-top", "20%" );
+  $( "#signup" ).show("fast");
 
 });
 $( "#click-signin-link" ).click(function() {
+  $(".box-signin").css( "margin-top", "30%" );
 	$( "#signup" ).hide("fast");
 
 	$( "#signin" ).show("fast");
