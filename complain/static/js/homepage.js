cb17eb32b8da2c27d3f01324d1b3dbb7eeee140d
Bibekpandey <spirit_bibek@yahoo.com>
2016-04-15 21:33:50 +0545

    fixed tags bug in profile page

--- complain/static/js/homepage.js
+++ complain/static/js/homepage.js
@@ -10,14 +10,6 @@ $("#comment-click").click(function(){
 	$("#display-form").show("fast");
 });
 
-$(document).ready(function(){
-  $('.notifications').popover({ 
-    html : true,
-    content: function() {
-      return $('#popover_notification_content').html();
-    }
-  });
-});
 
 $('#myModal').on('shown.bs.modal', function () {
   $('#myModal').appendTo("body").modal('show');
