b0843517e15ce38404061f836d5c98763a65377e
Bibekpandey <spirit_bibek@yahoo.com>
2016-04-16 01:03:25 -0400

    added csrf exempt in webhook view

--- complain/views/webhook.py
+++ complain/views/webhook.py
@@ -1,6 +1,8 @@
+from django.views.decorators.csrf import csrf_exempt
 import os
 import time
 
+@csrf_exepmt
 def hook(request):
 	if request.method=="POST":
 		os.system('echo hook obtained >> /tmp/hook')
